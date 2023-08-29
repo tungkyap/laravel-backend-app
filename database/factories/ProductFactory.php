fdad4d8bc32effa734b2bd03aa7c6a610442f8f0
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2023-08-29 10:37:13 +0700

    API CRUD category, product, auth login with laravel policy

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->sentence(2),
+            'description' => fake()->text,
+            'price' => fake()->randomFloat(2,0,100),
+            'image_url' => fake()->imageUrl($width=200, $height=200),
+
+        ];
+    }
+}
