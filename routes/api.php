241c46a9b8a89118d3389c0ffd4047bad69f3d07
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2023-08-30 14:45:56 +0700

    payment gateway midtrans, order, order item, callback services

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CallbackController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UploadController;
 use Illuminate\Http\Request;
@@ -27,7 +29,10 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::post('image/upload', [UploadController::class, 'uploadImage'])->middleware('auth:sanctum');;
-Route::post('image/upload-multiple', [UploadController::class, 'uploadMultipleImages'])->middleware('auth:sanctum');;
+Route::post('image/upload', [UploadController::class, 'uploadImage'])->middleware('auth:sanctum');;
+Route::post('orders', [OrderController::class, 'order'])->middleware('auth:sanctum');;
+
+Route::post('midtrans/notification/handling', [CallbackController::class, 'callback']);
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
